40357e64f3756d822e04b013901218c9c60eb159
narenbakshi97 <narenbakshi97@gmail.com>
2018-11-22 10:37:13 +0530

    22 Nov 2018

--- index.php
+++ index.php
@@ -7,31 +7,41 @@
 	<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.1/p5.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.1/addons/p5.dom.min.js"></script>
-	<script src="flower.js"></script>
-	<script src="bonsai.js"></script>
-	<script src="trunk.js"></script>
+	<script src="js/flower.js"></script>
+	<script src="js/bonsai.js"></script>
+	<script src="js/trunk.js"></script>
 	<title>Cherrish memory</title>
 
 	<style>
 		body{
-			overflow-x: hidden;
-			background: url("bg1.jpg");
+			background: url("sprites/bg1.jpg");
 			background-repeat: no-repeat;
 			background-size: cover;
 		}
-		.stitched {
-		   padding: 20px;
-		   margin: 5px;
-		   background: #020;
-		   color: #DAA520;
-		   font-weight: bold;
-		   line-height: 1.3em;
-		   border: 2px dashed #DAA520;
-		   border-radius: 10px;
-		   box-shadow: 0 0 0 4px #000, 2px 1px 6px 4px rgba(10, 10, 0, 0.5);
-		   text-shadow: -1px -1px #aa3030;
-		   font-weight: normal;
-		   cursor:pointer;
+
+		.form-group{
+			margin:20px;
+		}
+		.label{
+			margin:10px;
+			font-weight:bold;
+			text-transform: uppercase;
+			font-size:25px;
+			color:#ed225d;
+		}
+		input, textarea{
+			padding:10px 30px;
+			font-size:20px;
+			color:#ed225d;
+		}
+		:hover .btn-pink{
+			color:#fff;
+			background:rgb(255,105,180);
+		}
+		.btn-pink{
+			color:#eee;
+			background:rgb(255,20,147);
+			padding:10px 20px;
 		}
 		h3{
 			display: inline;
@@ -39,7 +49,7 @@
 		#formDiv{
 			font-family: 'Shadows Into Light', cursive;
 			height:610px;
-			width:1000px;
+			width:550px;
 			background-color:rgba(255,255,255,0.5);
 			color:#ed225d;
 		}
@@ -68,29 +78,6 @@
 		    max-width: 450px;
 		    max-height:500px;
 		}
-		.form-group{
-			margin:20px;
-		}
-		.label-control{
-			margin:10px;
-			font-weight:bold;
-			text-transform: uppercase;
-			font-size:25px;
-		}
-		.form-control input, .form-control textarea{
-			padding:10px 30px;
-			font-size:20px;
-			color:#ed225d;
-		}
-		:hover .btn-pink{
-			color:#fff;
-			background:rgb(255,105,180);
-		}
-		.btn-pink{
-			color:#eee;
-			background:rgb(255,20,147);
-			padding:10px 20px;
-		}
 		#memoryDiv button{
 			margin-top:200px;
 			position:absolute;
@@ -105,15 +92,14 @@
 </head>
 <body class="container-fluid">
 	<div id="tree"></div>
-
 </body>
-<script type="text/javascript">
+<script>
 	function newFlower(){
 		console.log("called");
 		memoryFlowers = [];
 		<?php
 		$db = mysqli_connect("localhost","root","","bonsai"); //keep your db name
-		$sql = "SELECT * FROM mem";
+		$sql = "SELECT * FROM flowers";
 		$result = $db->query($sql);
 		while($row = $result->fetch_assoc()) {
         ?>
@@ -159,12 +145,13 @@
 	    $imagename= $_FILES["userfile"]["name"]; 
 	    //echo $imagename;
 	    $imagetmp 	= (file_get_contents($_FILES['userfile']['tmp_name']));
-	    $stmt = $dbh->prepare("INSERT INTO mem(title,memory,date,time,image) VALUES(?,?,?,?,?)");
+	    $stmt = $dbh->prepare("INSERT INTO `flowers`(title,memory,date,time,image) VALUES(?,?,?,?,?)");
 	    $stmt->bindParam(1,$title);
 	    $stmt->bindParam(2,$memory);
 	    $stmt->bindParam(3,$date);
 	    $stmt->bindParam(4,$time);
 	    $stmt->bindParam(5,$imagetmp);
+
 	    $stmt->execute();
 	    if($stmt){
 	    	echo "<script>alert('Memory added!');</script>";
--- js/bonsai.js
+++ js/bonsai.js
@@ -1,16 +1,23 @@
 //the canvas
 var cnv;
 
+// the trunk
+var treeTrunk;
+
+
 // the images
 var pot,trunk,flower,plus,close,base;
 
-// the blossoms and the trunk
-var memoryFlowers = [], tableData = [], flowerSize = 50, c1, c2;
-
-var treeTrunk;
-
 //Inputs for the 6 fields
 var button, i1,i2,i3,i4,i5,i6;
+
+// flowers array
+// the blossoms and the trunk
+var memoryFlowers = [], flowerSize = 50;
+
+
+// close button
+var c1, c2;
 
 // Close those forms
 var formOpen, memoryOpen, formDiv, memoryDiv;
@@ -21,41 +28,39 @@ var plusY = 450;
 var plusLength = 110;
 
 function preload(){
-	base = loadImage("base.png")
-	pot = loadImage("pot.png");
-	trunk = loadImage("trunk.png");
-	flower = loadImage("flower.png");
-	plus = loadImage("plus.png");
-	close = loadImage("close.png");
+	base = loadImage("../memory_bonsai/sprites/base.png")
+	pot = loadImage("sprites/pot.png");
+	trunk = loadImage("sprites/trunk.png");
+	flower = loadImage("sprites/flower.png");
+	plus = loadImage("sprites/plus.png");
+	close = loadImage("sprites/close.png");
 }
+
 function setup(){
 	cnv = createCanvas(windowWidth, windowHeight);
 	cnv.parent("tree");
 	cnv.style('margin-left','-15px');
 	cnv.style('margin-top','5%');
   	cnv.style('background','transparent');
-	//init trunk
-	newFlower();
-	treeTrunk = new Trunk();
+  	// new flower called
+  	newFlower();
 
-	formOpen = false;
-	memoryOpen = false;
+  	treeTrunk = new Trunk();
+  	formOpen = false;
+  	memoryOpen = false;
 
-	c1 = new CloseButton();
-	c2 = new CloseButton();
+
+  	c1 = new CloseButton();
+  	c2 = new CloseButton();
 }
+
 function draw(){
 	clear();
 	image(pot,width/4,410,pot.width/1.5,pot.height/1.5);
 	image(base,0,pot.height/1.5+390,width,base.height/1.5);
+
 	treeTrunk.show();
 	treeTrunk.checkTrunk(mouseX,mouseY,plusX,plusY,plusLength);
-
-	//The flowers
-	for(let i = 0; i < memoryFlowers.length; i++){
-		memoryFlowers[i].show();
-	}
-
 
 	if(formOpen == true){
 		c1.show();
@@ -64,18 +69,27 @@ function draw(){
 	if(memoryOpen == true){
 		c2.show();
 	}
+
+
+	//The flowers
+	for(let i = 0; i < memoryFlowers.length; i++){
+		memoryFlowers[i].show();
+	}
+
+
 }
 
 function mousePressed(){
 	if(dist(mouseX,mouseY,plusX,plusY) < plusLength){
 		formOpen = true;
 		formDiv = null;
-		formDiv = createDiv("<form method='post' enctype='multipart/form-data'><div class='form-group'><div class='label-control'>Title</div><div class='form-control'><input type='text' name='title' placeholder='The title of the memory' required/></div></div>  <div class='form-group'><div class='label-control'>Date</div><div class='form-control'><input type='date' name='date' placeholder='The date' required/></div></div>   <div class='form-group'><div class='label-control'>Time</div><div class='form-control'><input type='time' name='time' placeholder='The time' required/></div></div>     <div class='form-group'><div class='label-control'>Picture</div><div class='form-control'><input type='file' name='userfile' required/></div></div>  <div class='form-group'><div class='label-control'>Memory</div><div class='form-control'><textarea name='memory' placeholder='Tell us all about that day!'></textarea></div></div> <div class='form-group'><div class='form-control'><input class='btn btn-pink btn-block' type='submit' value='submit' name='submit'/></div></div></form>");
+		formDiv = createDiv("<form method='post' enctype='multipart/form-data'><div class='form-group'><div class='label'>Title</div><div><input type='text' name='title' placeholder='The title of the memory' required/></div></div>  <div class='form-group'><div class='label'>Date</div><div><input type='date' name='date' placeholder='The date' required/></div></div>   <div class='form-group'><div class='label'>Time</div><div><input type='time' name='time' placeholder='The time' required/></div></div>     <div class='form-group'><div class='label'>Picture</div><div><input type='file' name='userfile' required/></div></div>  <div class='form-group'><div class='label'>Memory</div><div><textarea name='memory' placeholder='Tell us all about that day!'></textarea></div></div> <div class='form-group'><div><input class='btn btn-pink btn-block' type='submit' value='submit' name='submit'/></div></div></form>");
 		formDiv.id("formDiv");
 		formDiv.addClass("col-md-12");
 		formDiv.position(0,0);
-	}	
+	}
 
+	// flower click check
 	for(let i = 0; i < memoryFlowers.length; i++){
 		memoryFlowers[i].checkClicked(mouseX,mouseY);
 	}
@@ -93,10 +107,11 @@ function mousePressed(){
 			memoryOpen = false;
 		}
 	}
+
 }
 
 function CloseButton(){
-	this.x = width-100;
+	this.x = width-200;
 	this.y = 0;
 	this.r = 100;
 	this.show = function(){
